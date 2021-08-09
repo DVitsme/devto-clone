4d742dbd6cd1029bdf0be185504a790bca5468de
DVitsme <derrick.valentine@gmail.com>
2021-08-09 14:26:38 -0400

    navbar renders based off firebase auth

--- components/Navbar/AuthConditional.js
+++ components/Navbar/AuthConditional.js
@@ -1,5 +1,77 @@
-const AuthConditional = () => {
-  const { username, user } = {};
+// LOGIC: check if mobile then render based on if the user is logged in or not components are below
+
+import { useContext } from 'react';
+import Link from 'next/link';
+import toast from 'react-hot-toast';
+
+import UserContext from '../../context/userContext';
+
+const AuthConditional = ({ isMobile }) => {
+  const { user, username } = useContext(UserContext);
+
+  if (isMobile) {
+    return (
+      <>
+        {username ? (
+          <MobileIsAuth user={user} username={username} />
+        ) : (
+          <MobileNotAuth />
+        )}
+      </>
+    );
+  } else {
+    return (
+      <>
+        {user ? (
+          <DesktopIsAuth user={user} username={username} />
+        ) : (
+          <DesktopNotAuth />
+        )}
+      </>
+    );
+  }
+};
+
+const DesktopIsAuth = ({ user, username }) => {
+  return (
+    <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
+      <Link href="/admin">
+        <a className="whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">
+          Create Posts
+        </a>
+      </Link>
+      <div className="flex-shrink-0">
+        <Link href={`/${username || user.email}`}>
+          <a className="block relative ml-6">
+            <img
+              alt="profile"
+              src={user?.photoURL}
+              className="mx-auto object-cover rounded-full h-11 w-auto "
+            />
+          </a>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+const DesktopNotAuth = () => {
+  return (
+    <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
+      <Link href="/enter">
+        <a className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
+          Sign in
+        </a>
+      </Link>
+
+      <a className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">
+        Sign up
+      </a>
+    </div>
+  );
+};
+
+const MobileIsAuth = ({ user, username }) => {
   return (
     <div>
       <a
@@ -17,5 +89,22 @@ const AuthConditional = () => {
     </div>
   );
 };
-
+const MobileNotAuth = () => {
+  return (
+    <div>
+      <a
+        href="#"
+        className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
+      >
+        Sign up
+      </a>
+      <p className="mt-6 text-center text-base font-medium text-gray-500">
+        Existing customer?{' '}
+        <a href="#" className="text-indigo-600 hover:text-indigo-500">
+          Sign in
+        </a>
+      </p>
+    </div>
+  );
+};
 export default AuthConditional;
--- components/Navbar/Index.js
+++ components/Navbar/Index.js
@@ -1,4 +1,5 @@
 import { Fragment } from 'react';
+import Link from 'next/link';
 import { Popover, Transition } from '@headlessui/react';
 import {
   CubeTransparentIcon,
@@ -107,13 +108,15 @@ export default function Navbar() {
           <div className="max-w-7xl mx-auto px-4 sm:px-6">
             <div className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
               <div className="flex justify-start lg:w-0 lg:flex-1">
-                <a href="#">
-                  <span className="sr-only">Logo</span>
-                  <CubeTransparentIcon
-                    className="flex-shrink-0 h-10 w-10 text-indigo-600"
-                    aria-hidden="true"
-                  />
-                </a>
+                <Link href="/">
+                  <a>
+                    <span className="sr-only">Logo</span>
+                    <CubeTransparentIcon
+                      className="flex-shrink-0 h-10 w-10 text-indigo-600"
+                      aria-hidden="true"
+                    />
+                  </a>
+                </Link>
               </div>
               <div className="-mr-2 -my-2 md:hidden">
                 <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
@@ -200,19 +203,16 @@ export default function Navbar() {
                     </>
                   )}
                 </Popover>
-
-                <a
-                  href="#"
-                  className="text-base font-medium text-gray-500 hover:text-gray-900"
-                >
-                  Pricing
-                </a>
-                <a
-                  href="#"
-                  className="text-base font-medium text-gray-500 hover:text-gray-900"
-                >
-                  Docs
-                </a>
+                <Link href="/">
+                  <a className="text-base font-medium text-gray-500 hover:text-gray-900">
+                    Feed
+                  </a>
+                </Link>
+                <Link href="/enter">
+                  <a className="text-base font-medium text-gray-500 hover:text-gray-900">
+                    Docs
+                  </a>
+                </Link>
 
                 <Popover className="relative">
                   {({ open }) => (
@@ -309,20 +309,7 @@ export default function Navbar() {
                   )}
                 </Popover>
               </Popover.Group>
-              <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
-                <a
-                  href="#"
-                  className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900"
-                >
-                  Sign in
-                </a>
-                <a
-                  href="#"
-                  className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
-                >
-                  Sign up
-                </a>
-              </div>
+              <AuthConditional />
             </div>
           </div>
 
@@ -402,7 +389,7 @@ export default function Navbar() {
                       </a>
                     ))}
                   </div>
-                  <AuthConditional />
+                  <AuthConditional isMobile={true} />
                 </div>
               </div>
             </Popover.Panel>
